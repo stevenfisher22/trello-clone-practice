b25471f0bfb3fae2fa8fc19219446cc39c649877
Steven Fisher <steven.fisher22@gmail.com>
2019-03-21 10:28:21 -0500

    everything added and showing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,62 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function App() {
-    return (
-        <div>
-            Test
-        </div>
-    )
-}
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Card = ({items}) => (
+    <div className="card">
+        <Title />
+        <Ellipsis />
+        {items.map(item => (
+            <ListItem key={item.id} title={item.title} />
+        ))}
+        <AddButton />
+    </div>
+);
+
+
+const ListItem = ({title}) => (
+    <div>
+        {title}
+    </div>
+)
+
+
+const Title = () => (
+    <div>
+        Phone Features
+    </div>
+);
+
+
+const Ellipsis = () => (
+    <div>
+        ...
+    </div>
+);
+
+
+const AddButton = () => (
+    <div>
+        Add a card...
+    </div>
+);
+
+
+// Fake Data
+const Items = [
+    {
+        id: 1, 
+        title: 'Subwoofer'
+    },
+    {
+        id: 2, 
+        title: 'Wings'
+    },
+    {
+        id: 3, 
+        title: 'Seedless'
+    }
+];
+
+
+ReactDOM.render(<Card items={Items}/>, document.getElementById('root'))
